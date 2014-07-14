21340a957f41837c08712ee47b2cd3575acd1fb2
Mark Villacampa <markvjal@gmail.com>
2014-07-14 03:04:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# What is this
+
+This is a small ruby script that let's you run Rubymotion scripts in the popular app [CodeRunner](http://krillapps.com/coderunner/).
+
+It is very useful to quickly test some piece of code using a new Cocoa framework that you're learning or to try to reproduce a bug that you're experiencing.
+
+The script runs as an OSX app, so you cannot load iOS-specific frameworks. However, OSX and iOS share a lot of frameworks that you can use.
+
+# How to install
+
+1. Clone this repository somewhere in your Mac.
+
+2. Open CodeRunner. Go to the  "Languages" tab inside Preferences.
+
+3. Add a new language called Rubymotion. Set syntax mode to Ruby and file extension to ".rb"
+
+4. Mark the "Language uses compilation script" checkbox and click "Edit Script..."
+
+5. A new file will open. Replace its contents with the following line. The path is the local path to the `motion-coderunner.rb` file in your Mac.
+
+  ruby "/Users/mark/src/motion-coderunner/motion-coderunner.rb" "$@"
+
+6. Create a new CodeRunner file, set the language to Rubymotion, and write some code.
+
+# Extra frameworks
+
+By default, the script will link agaisnt a set of default frameworks. However, you can click the "Custom Run" button, and add more frameworks by writing their names (space separated) in the "Compilation Flags" field.
---
+++ motion-coderunner.rb
@@ -0,0 +1,91 @@
+#!/bin/ruby
+
+raise "You don't seem to have RubyMotion installed" unless Dir.exist? "/Library/RubyMotion/"
+
+require '/Library/RubyMotion/lib/motion/version.rb'
+
+filename = ARGV[0]
+
+if Dir.exist? ARGV[2]
+  extra_frameworks = []
+  filedir = ARGV[2]
+else
+  extra_frameworks = ARGV[2].split(' ')
+  filedir = ARGV[3]
+end
+
+filepath = File.join(filedir, filename)
+
+osx_version = `sw_vers -productVersion`.strip.match(/((\d+).(\d+))/)[0]
+
+mrep = "MREP_THERE_IS_ONLY_ONE_FILE"
+
+frameworks_stubs = Dir.glob("/Library/RubyMotion/data/osx/#{osx_version}/MacOSX/*_stubs.o").map { |f| %Q{"#{f}"} }.join(' ')
+bs_frameworks = Dir.glob("/Library/RubyMotion/data/osx/#{osx_version}/MacOSX/*.bridgesupport").map { |f| "--uses-bs #{f}"}.join(' ')
+
+#########################
+#### BUILD ruby file ####
+#########################
+
+env = %Q{ /usr/bin/env VM_PLATFORM="MacOSX" VM_KERNEL_PATH="/Library/RubyMotion/data/osx/#{osx_version}/MacOSX/kernel-x86_64.bc" VM_OPT_LEVEL="0" /usr/bin/arch -arch x86_64 }
+system %Q{ #{env} /Library/RubyMotion/bin/ruby #{bs_frameworks} --emit-llvm "#{filepath}.x86_64.s" #{mrep} "#{filepath}" }
+
+system %Q{ clang -fexceptions -c -arch x86_64 "#{filepath}.x86_64.s" -o "#{filepath}.x86_64.o" }
+
+#######################
+#### BUILD main.mm ####
+#######################
+
+main_txt = <<EOS
+#import <Foundation/Foundation.h>
+extern "C" {
+    void rb_define_global_const(const char *, void *);
+    void rb_rb2oc_exc_handler(void);
+    void rb_exit(int);
+    void ruby_init(void);
+    void ruby_init_loadpath(void);
+    void ruby_script(const char *);
+    void *rb_vm_top_self(void);
+    void #{mrep}(void *, void *);
+}
+
+int
+main(int argc, char **argv)
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    static bool initialized = false;
+    if (!initialized) {
+        ruby_init();
+        ruby_init_loadpath();
+        if (argc > 0) {
+            const char *progname = argv[0];
+            ruby_script(progname);
+        }
+        void *self = rb_vm_top_self();
+
+        rb_define_global_const(\"RUBYMOTION_ENV\", @\"development\");
+        rb_define_global_const(\"RUBYMOTION_VERSION\", @\"#{Motion::Version}\");
+
+        #{mrep}(self, 0);
+
+        initialized = true;
+    }
+
+    [pool release];
+    rb_exit(0);
+    return 0;
+}
+EOS
+
+File.open("main.mm", 'w') { |io| io.write(main_txt) }
+system "clang++ main.mm -o main.o -arch x86_64 -O0 -fexceptions -fblocks -fmodules -c"
+
+#########################
+#### LINK EXECUTABLE ####
+#########################
+
+frameworks = %W{AppKit Foundation CoreGraphics CoreServices ApplicationServices AudioToolbox AudioUnit CoreData QuartzCore Security CoreAudio DiskArbitration OpenGL ImageIO CoreText CoreFoundation CFNetwork SystemConfiguration IOSurface Accelerate CoreVideo}.concat(extra_frameworks)
+frameworks_flags = frameworks.map { |f| "-framework #{f}"}.join(' ')
+
+system %Q{ clang++ -o "#{filename}" main.o #{bs_frameworks} "#{filepath}.x86_64.o" -arch x86_64 -L/Library/RubyMotion/data/osx/#{osx_version}/MacOSX -lrubymotion-static -lobjc -licucore #{frameworks_flags} }
+system "./#{filename}"
